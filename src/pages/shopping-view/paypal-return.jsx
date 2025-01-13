6c177d5679200d38d4ec8a65cc59589098d9edd1
Kithmini Ekanayake <71839150+Kit2-netizen@users.noreply.github.com>
2025-01-13 07:23:46 +0530

    fix

--- src/pages/shopping-view/paypal-return.jsx
+++ src/pages/shopping-view/paypal-return.jsx
@@ -8,7 +8,9 @@ function PaypalReturnPage() {
   const dispatch = useDispatch();
   const location = useLocation();
   const params = new URLSearchParams(location.search);
+  
   const paymentId = params.get("paymentId");
+
   const payerId = params.get("PayerID");
 
   useEffect(() => {
